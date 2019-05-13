bacc61744c1d2dccf64b841c75113848c34e00d6
Bruno Sato <bruno.sato@live.com>
2019-05-13 17:06:02 -0300

    keys

--- packages/server/helper/constants.ts
+++ packages/server/helper/constants.ts
@@ -1,4 +1,4 @@
 export const JWT_KEY =
-  process.env.QUIDD_ENV === "prod"
-    ? "a324dd15-74c5-44ea-8f64-8f0e6b90844c"
-    : "af0464bb-b826-46b1-b838-e9f60b56651e";
+  process.env.MAPPER_ENV === "prod"
+    ? "18f6debd-f503-4a07-8303-4495d73f67ed"
+    : "d885aa62-4746-40ac-a4b5-c150817ad026";
